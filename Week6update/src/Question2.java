15652edcfd006d9d9e94c48b32debdb448f7bf26
Perishblue <visnudasdarshan@gmail.com>
2024-04-07 22:04:14 +0100

    Push 1 message in to repo

---
+++ Week6update/src/Question2.java
@@ -0,0 +1,15 @@
+public class Question2 {
+   // Write a Java program to divide two numbers and print on the screen.
+    // Test Data: 50/3
+    // Expected Output: 16
+
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        int num = 50;
+        int num1 = 3;
+        int divide = num / num1; // Display the Result as Expected Output
+
+        System.out.println("Expected Output: "+ divide);
+
+    }
+}
