cc2008e619bafa7e6ac34401450b7a18c6d1ca17
Perishblue <visnudasdarshan@gmail.com>
2024-04-07 22:27:01 +0100

    Push 2 message in to repo

--- Week6update/src/Question3.java
+++ Week6update/src/Question3.java
@@ -10,5 +10,22 @@ public class Question3 {
     //                  19
     //                  13
 
+    public static void main(String[] args) {
+        // Calculate and store the result of the expression: -5 + 8 * 6
+        int w = -5 + 8 * 6;
+
+        // Calculate and store the result of the expression: (55 + 9) % 9
+        int x = (55 + 9) % 9;
+
+        // Calculate and store the result of the expression: 20 + (-3 * 5 / 8)
+        int y = 20 + (-3 * 5 / 8);
+
+        // Calculate and store the result of the expression: 5 + 15 / 3 * 2 - 8 % 3
+        int z = 5 + 15 / 3 * 2 - 8 % 3;
+
+        // Print the calculated values, each on a new line
+        System.out.println("Expected Output : "+w + "\n" + x + "\n" + y + "\n" + z);
+
+    }
 
 }
