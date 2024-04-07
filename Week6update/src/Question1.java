c22eaf8f8ca138e626c0651773724017b0d7c282
Perishblue <visnudasdarshan@gmail.com>
2024-04-07 21:38:40 +0100

    my first lession to push

---
+++ Week6update/src/Question1.java
@@ -0,0 +1,13 @@
+public class Question1 {
+    //Write a Java program to print the sum of two numbers.
+   // Test Data: 74 + 36
+    //Expected Output: 110
+    public static void main(String[] args) {
+
+        int num1 = 74;
+        int num2 = 36;
+        int sum = num1 + num2; // Display the result Expected Outputs
+        System.out.println("Expected Outputs: "+sum);
+
+    }
+}
