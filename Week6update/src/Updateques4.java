99d161acbc803323c6b7dcbf3969a7a9a52a5f27
Perishblue <visnudasdarshan@gmail.com>
2024-04-07 22:52:54 +0100

    Push 2 message in to repo

--- Week6update/src/Updateques4.java
+++ Week6update/src/Updateques4.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Updateques4 {
+   // Write a Java program that takes two numbers as input and display the
+    //product of two numbers.
+    //Test Data:
+    //Input first number: 25
+    //Input second number: 5
+    //Expected Output : 25 x 5 = 125
+
     public static void main(String[] args) {
         // Create a Scanner object to read input from the user
+        Scanner in = new Scanner(System.in);
+
+        // Prompt the user to input the first number
+        System.out.print("Input first number: ");
+        // Read and store the first number
+        int num1 = in.nextInt();
+
+        // Prompt the user io input the second number
+        System.out.print("Input second number: ");
+        // Read and store the second number
+
+
+
 
     }
 }
