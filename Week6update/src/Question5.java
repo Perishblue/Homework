6566276fb72402ae0eeba5b1ec6bfa2b311d1f89
Perishblue <visnudasdarshan@gmail.com>
2024-04-07 23:12:28 +0100

    Push 1 message in to repo

---
+++ Week6update/src/Question5.java
@@ -0,0 +1,15 @@
+public class Question5 {
+
+
+    public static void main(String[] args) {
+
+        // Multipal by number
+
+        int num = 8;
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.printf("%d * %d \n" , num, i, num * i);
+
+        }
+    }
+}
