b596763dbfc7f58cac55d902694bb001406cd26e
Perishblue <visnudasdarshan@gmail.com>
2024-04-07 22:39:03 +0100

    Push 1 message in to repo

---
+++ Week6update/src/Question4.java
@@ -0,0 +1,18 @@
+public class Question4 {
+    // Write a Java program that takes two numbers as input and display the
+    // product of two numbers.
+    // Test Data:
+    // Input first number: 25
+    // Input second number: 5
+    // Expected Output : 25 x 5 = 125
+
+    // Declare and initialize static variables x and y
+    static int x = 25;
+    static int y = 5;
+
+    public static void main(String[] args) {
+        // Calculate and print the product of x and y
+        System.out.println(x * y);
+
+    }
+}
