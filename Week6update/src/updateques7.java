9265b529c02219fc6565a440fa2102031aea00e8
Perishblue <visnudasdarshan@gmail.com>
2024-04-08 21:06:38 +0100

    Push 1 message in to repo

---
+++ Week6update/src/updateques7.java
@@ -0,0 +1,22 @@
+public class updateques7 {
+    //  Write program to display
+    // Test Data: ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+    // Expected Output 2.138888888888889
+
+    public static void main(String[] args) {
+        // using float method
+
+        float a = 25.5f;
+        float b = 3.5f;
+        float c = 3.5f;
+        float d = 3.5f;
+        float e = 40.5f;
+        float f = 4.5f;
+
+        System.out.println((a * b - c * d) / (e - f));
+
+
+
+
+    }
+}
