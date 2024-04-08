3b673c205ef5688560511087c7654fcdc0673dcb
Perishblue <visnudasdarshan@gmail.com>
2024-04-08 23:07:37 +0100

    Push 1 message in to repo

---
+++ Week6update/src/InstanceVariables/Question3.java
@@ -0,0 +1,18 @@
+package InstanceVariables;
+
+public class Question3 {
+    // 1. Write a Java programme using the following steps.
+    // 1.3 Call both instance variables into the instance method inside the print statement.
+
+    String name = "darshan";
+    String name1 = "visnudas";
+    // instance variable
+    public static void main(String[] args) {
+        Question3 a = new Question3();
+
+        System.out.println(a.name);
+        System.out.println(a.name1);
+
+
+    }
+}
