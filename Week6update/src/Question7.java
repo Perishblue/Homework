1399d3db99d043e254b0e515ab41ad8d3ce95ce8
Perishblue <visnudasdarshan@gmail.com>
2024-04-08 20:04:32 +0100

    Push 1 message in to repo

--- Week6update/src/Question7.java
+++ Week6update/src/Question7.java
@@ -5,6 +5,7 @@ public class Question7 {
 
     public static void main(String[] args) {
         // Calculate and print the result of a mathematical expression
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
 
     }
 }
