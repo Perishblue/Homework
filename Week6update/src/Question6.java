dee18ea57e7d973306b8c9b3f2c98e540c787c82
Perishblue <visnudasdarshan@gmail.com>
2024-04-08 16:47:36 +0100

    Push 1 message in to repo

---
+++ Week6update/src/Question6.java
@@ -0,0 +1,10 @@
+public class Question6 {
+             // Write a Java program to display. Expected Output
+             // 25 != 39
+             // 25 < 39
+             // 25 <= 39
+             public static void main(String[] args) {
+
+             }
+
+}
