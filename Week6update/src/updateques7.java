b706b384af013a70f2bed438d49442ee21dea3d1
Perishblue <visnudasdarshan@gmail.com>
2024-04-08 21:07:35 +0100

    Push 2 message in to repo

--- Week6update/src/updateques7.java
+++ Week6update/src/updateques7.java
@@ -8,12 +8,10 @@ public class updateques7 {
 
         float a = 25.5f;
         float b = 3.5f;
-        float c = 3.5f;
-        float d = 3.5f;
         float e = 40.5f;
         float f = 4.5f;
 
-        System.out.println((a * b - c * d) / (e - f));
+        System.out.println((a * b - b * b) / (e - f));
 
 
 
