359958a4c71f5daa1f38b77c7698e69555f1543a
Perishblue <visnudasdarshan@gmail.com>
2024-04-08 22:43:33 +0100

    Push 1 message in to repo

---
+++ Week6update/src/InstanceVariables/Question1.java
@@ -0,0 +1,12 @@
+package InstanceVariables;
+
+public class Question1 {
+   // 1. Write a Java programme using the following steps.
+   // 1.1 Declare two instance variables.
+
+    String name = "darshan";
+    String name1 = "visnudas";
+    String name2 = "name + name1";
+
+
+}
