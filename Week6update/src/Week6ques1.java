5d7d6ec0f159d211431007197063c5475c2ea3f3
Perishblue <visnudasdarshan@gmail.com>
2024-04-08 22:33:06 +0100

    Push 2 message in to repo

---
+++ Week6update/src/Week6ques1.java
@@ -0,0 +1,21 @@
+public class Week6ques1 {
+     // Write a Java programme using the following steps.
+     // 1.1 Declare two instance variables.
+     // 1.2 Declare one instance method.
+     // 1.3 Call both instance variables into the instance method inside the print statement.
+     // 1.4 Declare the Main method.
+     // 1.5 Call the above instance method into the Main method and run the programme.
+
+    String name = "darshan";  // global variables
+    String name2 = "visnudas"; // global variables
+    public static void main(String[] args) {
+
+        Week6ques1 d = new Week6ques1();
+
+        System.out.println(d.name);
+        System.out.println(d.name2);
+
+    }
+
+
+}
