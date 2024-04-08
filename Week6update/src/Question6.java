7f513503fe19b16b700583311cd3d0ca94f6825d
Perishblue <visnudasdarshan@gmail.com>
2024-04-08 16:51:52 +0100

    Push 2 message in to repo

--- Week6update/src/Question6.java
+++ Week6update/src/Question6.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Question6 {
              // Write a Java program to display. Expected Output
              // 25 != 39
              // 25 < 39
              // 25 <= 39
              public static void main(String[] args) {
+         // Create scanner to input from the command window
+                 Scanner input = new Scanner(System.in);
+                 int num1; // Input first number
+                 int num2; // Input second number
+
+                 // Input the first integer
 
              }
 
