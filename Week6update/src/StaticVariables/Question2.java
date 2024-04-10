b2804e3f2015c3683f1a62435360b2a154a2b506
Perishblue <visnudasdarshan@gmail.com>
2024-04-10 09:39:23 +0100

    Push 1 message in to repo

---
+++ Week6update/src/StaticVariables/Question2.java
@@ -0,0 +1,13 @@
+package StaticVariables;
+
+public class Question2 {
+    // Write a Java programme using the following steps.
+    // Declare one static method.
+
+    public static void main(String[] args) { name(); }
+
+        public static void name() {
+            System.out.println("perish blue");
+
+    }
+}
---
+++ Week6update/src/StaticVariables/Question3.java
@@ -0,0 +1,26 @@
+package StaticVariables;
+
+public class Question3 {
+    // Write a Java programme using the following steps.
+    // Call both static variables into the static method inside the print statement.
+
+    static String name = "perish";
+    static String name1 = "blue";
+
+    public static void main(String[] args) {
+        System.out.println(name);
+        System.out.println(name1);
+        Question3 a = new Question3();
+        a.m1();
+
+
+    }
+
+    public void m1() {
+        String course = "java";
+        System.out.println(course);
+        System.out.println(name);
+        System.out.println(name1);
+
+    }
+}
