6cdda8cfd4e362791b882f91e215435c165f3829
Perishblue <visnudasdarshan@gmail.com>
2024-04-10 09:18:39 +0100

    Push 2 message in to repo

--- Week6update/src/StaticVariables/Question1.java
+++ Week6update/src/StaticVariables/Question1.java
@@ -4,11 +4,13 @@ public class Question1 {
     // Write a Java programme using the following steps.
     // Declare two static variables.
 
-    static String name = "darshan";
+    static String name = "perish";
+    static String name1 = "blue";
 
     public static void main(String[] args) {
 
         System.out.println(name);
+        System.out.println(name1);
 
     }
 }
