431531c2a9d3acccbdabfa72f193bb9487207035
Perishblue <visnudasdarshan@gmail.com>
2024-04-10 08:39:05 +0100

    Push 1 message in to repo

---
+++ Week6update/src/InstanceVariables/Question6.java
@@ -0,0 +1,14 @@
+package InstanceVariables;
+
+public class Question6 {
+    // 1. Write a Java programme using the following steps.
+    // 1.4 Declare the Main method.
+
+
+    public static void main(String[] args) {
+        // Main method
+
+        System.out.println("darshan");
+        System.out.println("visnudas");
+    }
+}
