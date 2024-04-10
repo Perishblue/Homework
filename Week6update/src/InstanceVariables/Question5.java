7b4ab2f4269cac5374e925201828c7114a21c2b7
Perishblue <visnudasdarshan@gmail.com>
2024-04-10 09:06:05 +0100

    Push 1 message in to repo

---
+++ Week6update/src/InstanceVariables/Question5.java
@@ -0,0 +1,19 @@
+package InstanceVariables;
+
+public class Question5 {
+    // 1. Write a Java programme using the following steps.
+    // 1.5 Call the above instance method into the Main method and run the programme.
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+        Question5 a = new Question5();
+        a.name();
+
+    }
+
+    public void name() {
+        System.out.println("darshan");
+    }
+
+}
