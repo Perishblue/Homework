1c70fd42c2f2ce4065db87c4b2da89bdace81507
Perishblue <visnudasdarshan@gmail.com>
2024-04-11 09:27:43 +0100

    Push 3 message in to repo

--- Week6update/src/Answer5.java
+++ Week6update/src/Answer5.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Answer5 {
     // 5. Write a program for a calculator with addition, subtraction, multiplication, and division
     //methods all with parameters and use string concatenation methods. (Note: Two static
