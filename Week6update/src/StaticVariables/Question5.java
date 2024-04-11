f784bfb73462477f605564d59470663eb7fa0656
Perishblue <visnudasdarshan@gmail.com>
2024-04-11 08:52:09 +0100

    Push 1 message in to repo

---
+++ Week6update/src/StaticVariables/Question5.java
@@ -0,0 +1,14 @@
+package StaticVariables;
+
+public class Question5 {
+    // Write a Java programme using the following steps.
+    // Call the static method into the Main method and run the programme.
+
+    public static void main(String[] args) { name();
+
+    }
+    public static void name() {
+        System.out.println("perish blue");
+
+    }
+}
