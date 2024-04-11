c1b314615590ebbb08f3942aedd984188985973d
Perishblue <visnudasdarshan@gmail.com>
2024-04-11 09:57:58 +0100

    Push 3 message in to repo

---
+++ Week6update/src/Answer9.java
@@ -0,0 +1,12 @@
+public class Answer9 {
+
+   // Write a program to convert the upper case to lower case.
+
+    public static void main(String[] args) {
+        String txt = "PERISH BLUE";
+
+        System.out.println(txt. toLowerCase());
+
+
+    }
+}
