5b00347669bdb811e5bb94f44ce5b2434ba68c26
Perishblue <visnudasdarshan@gmail.com>
2024-04-14 22:14:10 +0100

    Push 1 message in to repo

---
+++ Week6update/src/week7homework/Answer2.java
@@ -0,0 +1,19 @@
+package week7homework;
+
+public class Answer2 {
+
+       //  Declare array and store any 5 countries and print them in console.
+  public static void main(String[] args) {
+
+      String[] country = {"India", "Spain", "London", "France", "Portugal"};
+
+      for (int a =0; a < country.length; a++){
+
+          System.out.println(country[a]);
+      }
+
+
+
+
+  }
+}
