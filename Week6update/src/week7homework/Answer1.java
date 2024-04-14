6dd2793be8bfafda38da312ceb203bc6e8d7a0a5
Perishblue <visnudasdarshan@gmail.com>
2024-04-14 21:36:52 +0100

    Push 1 message in to repo

---
+++ Week6update/src/week7homework/Answer1.java
@@ -0,0 +1,19 @@
+package week7homework;
+
+import java.util.Scanner;
+
+public class Answer1 {
+   /* Write a java program that tells us that Input number is odd or even? HINT: use ternary
+    operator (? :)*/
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int number = sc.nextInt();
+
+        // ternary operator to check number ? use for condition
+        String result = number%2==0 ? "Even" : "Odd";
+
+        System.out.println(number +"is "+ result);
+    }
+}
