b7c7491d3c40391b3414ce9463ac0269e92a4749
Perishblue <visnudasdarshan@gmail.com>
2024-04-14 23:36:44 +0100

    Push 1 message in to repo

--- Week6update/src/week7homework/Answer3.java
+++ Week6update/src/week7homework/Answer3.java
@@ -1,7 +1,23 @@
 package week7homework;
 
 public class Answer3 {
-   /* Declare multidimensional array and store 5 countries and their capital and print them in
-    console.*/
+    /* Declare multidimensional array and store 5 countries and their capital and print them in
+     console.*/
+    public static void main(String[] args) {
 
-}
+        String[][] country = {{"India, Delhi", "Spain, Madrid", "England, London", "France, Parish", "Portugal, Lisbon" },
+                {"India, Delhi", "Spain, Madrid", "England, London", "France, Parish", "Portugal, Lisbon" },
+                {"India, Delhi", "Spain, Madrid", "England, London", "France, Parish", "Portugal, Lisbon" },
+                {"India, Delhi", "Spain, Madrid", "England, London", "France, Parish", "Portugal, Lisbon" },
+                {"India, Delhi", "Spain, Madrid", "England, London", "France, Parish", "Portugal, Lisbon" },};
+
+
+
+        System.out.println(country[0][4]);
+        System.out.println(country[1][3]);
+        System.out.println(country[2][2]);
+        System.out.println(country[3][1]);
+        System.out.println(country[4][0]);
+            }
+
+        }
