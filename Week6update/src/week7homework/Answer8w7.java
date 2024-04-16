fea461fb29c836c41e0f93ba4fe92e036e13bf3b
Perishblue <visnudasdarshan@gmail.com>
2024-04-16 09:33:27 +0100

    Push 1 message in to repo

---
+++ Week6update/src/week7homework/Answer8w7.java
@@ -0,0 +1,17 @@
+package week7homework;
+
+public class Answer8w7 {
+    // Write a java program to get numbers from users and print whether it is positive or negative.
+
+    public static void main(String[] args) {
+
+        double number = 15.5;
+
+                if (number > 0) {
+                    System.out.println("number is positive");
+                }
+                else {
+                    System.out.println("number is negative");
+                }
+    }
+}
