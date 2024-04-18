b0c59abd63b8f8a6bfb1d301e59bf29e3bcbd6d3
Perishblue <visnudasdarshan@gmail.com>
2024-04-19 00:50:55 +0100

    Push 1 message in to repo

---
+++ Week6update/src/week7homework/Answer23w7.java
@@ -0,0 +1,10 @@
+package week7homework;
+
+public class Answer23w7 {
+
+    // Write java program using for loop and print your name 10 times.
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            System.out.println("Perishblue");
+    }
+}
