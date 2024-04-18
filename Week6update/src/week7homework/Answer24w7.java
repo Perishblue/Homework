67fd778d0683fd724507e125f2a436d880c95acb
Perishblue <visnudasdarshan@gmail.com>
2024-04-19 00:54:46 +0100

    Push 1 message in to repo

---
+++ Week6update/src/week7homework/Answer24w7.java
@@ -0,0 +1,11 @@
+package week7homework;
+
+public class Answer24w7 {
+
+    // Write java program using for loop and print surname 9 times.
+
+    public static void main(String[] args) {
+      for (int i = 0; i < 9; i++)
+        System.out.println("Baby shark");
+    }
+}
