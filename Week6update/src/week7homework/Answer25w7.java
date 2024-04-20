0ae80f32e8a0b0ed4c1e68b4c15853021a36280b
Perishblue <visnudasdarshan@gmail.com>
2024-04-20 21:52:58 +0100

    Push 2 message in to repo

---
+++ Week6update/src/week7homework/Answer25w7.java
@@ -0,0 +1,18 @@
+package week7homework;
+
+public class Answer25w7 {
+
+    // Declare one method with return type and parameter and print your name in console.
+
+
+    public static void main(String[] args) {
+
+         String name = "Persihblue";
+         printhellow(name);
+
+
+    }
+    static void printhellow (String name) {
+        System.out.println("Hello " + name);
+    }
+}
--- Week6update/src/week7homework/Answer26w7.java
+++ Week6update/src/week7homework/Answer26w7.java
@@ -15,7 +15,7 @@ public class Answer26w7 {
         for (int i = byteArray.length - 1,j =0; i >= 0; i--,j++) {
             reversedArray[j] = byteArray[i]; }
         String reversedString = new String(reversedArray);
-        System.out.println("Oringinal srtin: " + str);
+
         System.out.println("Reversed string: " + reversedString);
 
         }
